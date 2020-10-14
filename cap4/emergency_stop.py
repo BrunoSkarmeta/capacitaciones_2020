eaa14558f5c91086b3967307881c6f8cd0265966
Bruno Skarmeta <bskarmeta@gmail.com>
2020-10-14 03:30:55 -0300

    antes no funcionaba

--- cap4/emergency_stop.py
+++ cap4/emergency_stop.py
@@ -28,42 +28,25 @@ def mov_duckiebot(key):
 
 
 def det_duckie(obs):
-    lower_yellow = np.array([25,28,160])
-    upper_yellow = np.array([34,255,255])
-    min_area = 2500
     ### DETECTOR HECHO EN LA MISIÓN ANTERIOR
-    dets = list()
-    ImagenHSV = cv2.cvtColor(obs, cv2.COLOR_RGB2HSV)
-
+    lower_yellow = np.array([140, 140, 0])
+    upper_yellow = np.array([255, 255, 0])
     # Filtrar colores de la imagen en el rango utilizando
-
-    mask = cv2.inRange(ImagenHSV, lower_yellow, upper_yellow)
-
-    # Bitwise-AND entre máscara (mask) y original (obs) para visualizar lo filtrado
-
-    img_out = cv2.bitwise_and(ImagenHSV, ImagenHSV, mask = mask)
-
-        # Se define kernel para operaciones morfológicas
-    kernel = np.ones((5,5),np.uint8)
-
-    # Aplicar operaciones morfológicas para eliminar ruido
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    # Esto corresponde a hacer un Opening
-    # https://docs.opencv.org/trunk/d9/d61/tutorial_py_morphological_ops.html
-    #Operacion morfologica erode
-    mask_out = cv2.erode(mask, kernel, iterations = 1)
-    #Operacion morfologica dilate
-    mask_out = cv2.dilate(mask, kernel, iterations = 1)
-
+    Filtro = cv2.inRange(obs, lower_yellow, upper_yellow)
     # Busca contornos de blobs
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # https://docs.opencv.org/trunk/d3/d05/tutorial_py_table_of_contents_contours.html
+    contours, hierarchy = cv2.findContours(Filtro, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+
+    dets = list()
+
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
-        #Filtrar por area minima
-        AREA=h*w
-        if AREA > min_area: # DEFINIR AREA
-            #Dibujar rectangulo en el frame original
-            cv2.rectangle(obs, (x, y), (x+w,y+h), (255,0,0), 1)
+        AREA= w*h
+        if AREA > 400:
+            # En lugar de dibujar, se agrega a la lista
+            dets.append((x,y,w,h))
+
     return dets
 
 def draw_dets(obs, dets):
@@ -109,7 +92,7 @@ if __name__ == '__main__':
     duck_pos = np.array([2,0,2])
 
     # Constante que se debe calcular
-    C = 50 # f * dr (f es constante, dr es conocido)
+    C = 60.688 # f * dr (f es constante, dr es conocido)
 
     while True:
 
@@ -124,7 +107,8 @@ if __name__ == '__main__':
 
         # Si hay alerta evitar que el Duckiebot avance
         if alert:
-            action[0]=0
+            if key == 119:
+                action[0] = 0
 
         # Se ejecuta la acción definida anteriormente y se retorna la observación (obs),
         # la evaluación (reward), etc
